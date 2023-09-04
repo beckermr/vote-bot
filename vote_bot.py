8f5bfca3baae15b617dafdcc120db518304bb2a2
beckermr <becker.mr@gmail.com>
2023-09-04 01:20:26 -0500

    what is this

--- vote_bot.py
+++ vote_bot.py
@@ -47,7 +47,7 @@ def send_matrix_message(msg):
         "https://matrix-client.matrix.org",
         token=os.environ["MATRIX_TOKEN"]
     )
-    matrix.send_message(os.environ["MATRIX_ROOM"], "@room " + msg)
+    matrix.send_notice(os.environ["MATRIX_ROOM"], "@room\n" + msg)
 
 
 def read_config(fname):
