760a4061045bc3ff64d41374226b1dfe5f248209
beckermr <becker.mr@gmail.com>
2023-09-03 23:39:52 -0500

    WIP setup initial structure

---
+++ vote_bot.py
@@ -0,0 +1,8 @@
+# import yaml
+
+# from matrix_client.api import MatrixHttpApi
+
+# matrix = MatrixHttpApi("https://matrix.org", token="some_token")
+# response = matrix.send_message("!roomid:matrix.org", "Hello!")
+
+print("hellow world!")
