8a571eabfacffb3603a7dafa357911ac78da23ff
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-09-04 07:51:20 -0500

    WIP setup initial structure (#1)
    * WIP setup initial structure
    
    * try a test
    
    * try an email
    
    * try this again
    
    * do this again
    
    * try again
    
    * dumb bug
    
    * add blurb
    
    * what is this
    
    * this one
    
    * more stuff to make it real
    
    * try again
    
    * try this
    
    * try this
    
    * try again
    
    * more verbpse
    
    * try this
    
    * try again
    
    * print some info
    
    * docs
    
    * docs
    
    * docs

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # vote-bot
+[![tests](https://github.com/beckermr/vote-bot/actions/workflows/test.yaml/badge.svg)](https://github.com/beckermr/vote-bot/actions/workflows/test.yaml)
+
+A github action/bot to run conda-forge-style votes.
+
+## Usage
+
+In your repo create a GitHub Action like this
+
+```yaml
+name: vote-bot
+
+on:
+  schedule:
+    - cron: '23 15 * * *'
+  workflow_dispatch:
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.ref }}
+  cancel-in-progress: ${{ github.ref != 'refs/heads/main' }}
+
+jobs:
+  vote-bot:
+    name: vote-bot
+    runs-on: "ubuntu-latest"
+    steps:
+      - uses: actions/checkout@v3
+      - uses: beckermr/vote-bot@main
+        with:
+          matrix_token: ${{ secrets.MATRIX_TOKEN }}
+          matrix_room: ${{ secrets.MATRIX_ROOM }}
+          matrix_home_server: "https://matrix-client.matrix.org"
+          email: <email to send vote notices>
+          email_password: ${{ secrets.EMAIL_PASSWORD }}
+          # change these to match the email address sending your vote notices
+          email_smtp_server: smtp.gmail.com
+          email_smtp_port: 587
+          vote_notice_email: <destination email for vote notices>
+          vote_directory: votes
+          vote_defaults: |
+            org: conda-forge
+            duration: 10
+```
+
+Each vote is a YAML file with the contents:
+
+```yaml
+org: conda-forge  # name of org holding the vote
+start_time:  # unix timestamp of when vote starts
+            # make via python -c "import time; print(time.time())"
+duration: 10  # duration of vote in days
+title: "Vote to add foo to conda-forge"  # title of vote
+description: |  # description of vote
+This is a vote to add foo to conda-forge.  Please vote yes or no.
+```
---
+++ vote_bot.py
@@ -0,0 +1,147 @@
+import os
+import time
+import yaml
+import glob
+import copy
+import smtplib
+import textwrap
+from email.message import EmailMessage
+
+ONE_DAY = 86400
+
+
+MSG_TEMPLATE = """\
+Hello{salutation}!
+
+I am the friendly conda-forge-daemon vote bot. :)
+
+The vote '{title}' {timing}! Please check your email for instructions.
+
+See below for more information about this vote.
+
+Cheers,
+conda-forge-daemon vote bot
+
+vote details:
+{details}
+"""
+
+
+def send_email_message(body, title):
+    print(
+        """\
+sending email message:
+    from: %s
+    to: %s
+    subject: %s
+    content:
+%s
+""" % (
+            os.environ["EMAIL"],
+            os.environ["VOTE_NOTICE_EMAIL"],
+            title,
+            textwrap.indent(body, "        "),
+        ),
+        flush=True,
+    )
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = title
+    msg['From'] = os.environ["EMAIL"]
+    msg['To'] = os.environ["VOTE_NOTICE_EMAIL"]
+
+    s = smtplib.SMTP(
+        os.environ["EMAIL_SMTP_SERVER"],
+        int(os.environ["EMAIL_SMTP_PORT"]),
+    )
+    s.starttls()
+    s.login(os.environ["EMAIL"], os.environ["EMAIL_PASSWORD"])
+    s.send_message(msg)
+    s.quit()
+
+
+def send_matrix_message(msg):
+    print(
+        "sending matrix message:\n%s" % textwrap.indent(msg, "    "),
+        flush=True,
+    )
+    from matrix_client.api import MatrixHttpApi
+
+    matrix = MatrixHttpApi(
+        os.environ["MATRIX_HOME_SERVER"],
+        token=os.environ["MATRIX_TOKEN"]
+    )
+    matrix.send_message(os.environ["MATRIX_ROOM"], msg)
+
+
+def read_config(fname):
+    with open(fname, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+def process_config(config):
+    start_time = config["start_time"]
+    duration = int(config["duration"] * ONE_DAY)
+
+    end_time = start_time + duration
+    midpoint = start_time + duration / 2
+
+    curr_time = time.time()
+
+    org = config.get("org", "")
+    if org != "":
+        org = org + " "
+
+    # get message to send
+    title = None
+    timing = None
+    if curr_time - start_time >= 0 and curr_time - start_time <= 1.5 * ONE_DAY:
+        title = "%svote started: %s" % (org, config["title"])
+        timing = "has started"
+    elif curr_time - midpoint >= 0 and curr_time - midpoint <= 1.5 * ONE_DAY:
+        title = "%svote half-way done: %s" % (org, config["title"])
+        timing = "is half-way done"
+    elif end_time - curr_time <= 1.5 * ONE_DAY and end_time >= curr_time:
+        title = "%svote ending soon: %s" % (org, config["title"])
+        timing = "will end in approximately one dat"
+
+    if title is not None and timing is not None:
+        msg = MSG_TEMPLATE.format(
+            salutation=" @room",
+            title=config["title"],
+            timing=timing,
+            details=config["details"],
+        )
+        send_matrix_message(msg)
+        msg = MSG_TEMPLATE.format(
+            salutation="",
+            title=config["title"],
+            timing=timing,
+            details=config["details"],
+        )
+        send_email_message(msg, title)
+
+
+if __name__ == "__main__":
+    votes = glob.glob(os.path.join(os.environ["VOTE_DIRECTORY"], "*.yaml"))
+
+    if "VOTE_DEFAULTS" in os.environ:
+        default_config = yaml.safe_load(os.environ["VOTE_DEFAULTS"])
+
+    for vote in votes:
+        if os.path.basename(vote) != "defaults.yaml":
+            config = read_config(vote)
+            _config = copy.deepcopy(default_config)
+            _config.update(config)
+            print(
+                "processing vote in file %s:\n%s" % (
+                    vote,
+                    textwrap.indent(
+                        yaml.dump(_config, default_flow_style=False, indent=2),
+                        "    ",
+                    ),
+                ),
+                flush=True,
+            )
+            process_config(_config)
